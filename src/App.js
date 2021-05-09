de0398c2bc31128e9ae76783dfb73fcf76dc2eca
JeremieC <certain.j@live.fr>
2021-05-09 21:44:47 +0200

    example with markdown

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import React, {Component} from 'react';
+import { sampleText } from './sampleText';
+import marked from 'marked'
+
+class App extends Component {
+
+  state = {
+      text: sampleText
+  }
+
+  componentDidMount() {
+      const text = localStorage.getItem('text')
+      if (text) {
+          this.setState({ text })
+      } else {
+          this.setState({ text: sampleText})
+      }
+  }
+
+  componentDidUpdate() {
+      const { text } = this.state
+      localStorage.setItem('text', text)
+  }
+
+
+    //Event Textarea value
+  handleChange = event => {
+      const text = event.target.value
+      this.setState({ text })
+  }
+
+  //Use the mark library, and return as an object
+    //Destructuré objet { objet }
+  renderText = text => {
+      const __html = marked(text, { sanitize: true })
+      return { __html }
+  }
+
+  render() {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-sm-6'>
+                    <textarea
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                        className='form-control'
+                        rows='35' />
+                </div>
+                <div className='col-sm-6'>
+                    <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+                </div>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default App;
